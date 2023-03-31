b0e9b87dd41092dbd87a4ea353915c61b6cdd47e
Klaus <nikolai.borsin@mail.ru>
2023-03-31 22:13:35 +0300

    settingProject

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require('next-transpile-modules')(['tailwindcss'])
+
+const nextConfig = withTM({
   reactStrictMode: true,
-}
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        child_process: false,
+        fs: false,
+        module: false,
+      }
+    }
+
+    return config
+  },
+})
 
 module.exports = nextConfig
