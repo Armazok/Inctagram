b704db2d3baa7f9d8027fdd3833dc5993de269d1
Anna Galkina <anna.blackbird1@gmail.com>
2023-04-02 21:26:43 +0700

    feat: add forgot password.

--- src/pages/auth/forgot-password.tsx
+++ src/pages/auth/forgot-password.tsx
@@ -2,19 +2,15 @@ import React from 'react'
 
 import { NextPage } from 'next'
 
-
 import Link from '@/components/link/Link'
-
+import style from '@/pages/auth/register.module.scss'
+import containerBlock from '@/styles/container.module.scss'
 
 const ForgotPassword: NextPage = () => {
   return (
-    <div>
+    <div className={`${containerBlock.container} ${style.registerBlock}`}>
       <h2>Forgot Password</h2>
-      <Link
-        href={'/auth/sing-in'}
-        title={'Back to Sign In'}
-        // className={style.link}
-      />
+      <Link href={'/auth/sing-in'} title={'Back to Sign In'} className={style.desciptionSignIn} />
     </div>
   )
 }
