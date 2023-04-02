8b6cc4e76f64dead83a28433092ede952617fe58
Anna Galkina <anna.blackbird1@gmail.com>
2023-04-02 21:25:44 +0700

    feat: add validation for create new password.

--- src/pages/auth/create-new-password.tsx
+++ src/pages/auth/create-new-password.tsx
@@ -3,55 +3,83 @@ import React from 'react'
 import { NextPage } from 'next'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
-export interface CreateNewPasswordFormData {
-  password: string
-  passwordConfirmation: string
-}
+import style from '@/pages/auth/register.module.scss'
+import containerBlock from '@/styles/container.module.scss'
 
-const CreateNewPassword: NextPage = () => {
+// export interface CreateNewPasswordFormData {
+//   password: string
+//   passwordConfirmation: string
+// }
+
+
+const MAX_LENGTH = 20;
+const MIN_LENGTH = 6;
+
+const CreateNewPassword = () => {
   const {
     register,
     handleSubmit,
     watch,
     reset,
     formState: { errors },
-  } = useForm<CreateNewPasswordFormData>()
+  } = useForm({
+    defaultValues: {
+      password: '',
+      passwordConfirmation: '',
+    },
+  })
 
   const onSubmit: SubmitHandler<FieldValues> = data => {
     const { password, passwordConfirmation } = data
 
-    console.log(data)
+    console.log(errors)
     reset()
   }
 
   return (
-    <div>
-      <h2>Create New Password</h2>
+    <div
+        // className={`${containerBlock.container} ${style.registerBlock}`}
+    >
+      <div className={style.formContainer}>
+        <h2>Create New Password</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/*<label>New password</label>*/}
-        {/*<input {*/}
-        {/*    ...register('password',*/}
-        {/*        {required:*/}
-        {/*                { value: true, message: 'This is required' },*/}
-        {/*            // maxLength: MAX_LENGTH,*/}
-        {/*            // minLength: MIN_LENGTH*/}
-        {/*        })}  />*/}
-        {/*<div>{<span>{errors.password?.message}</span>}</div>*/}
-        {/*<label>Password confirmation</label>*/}
-        {/*<input {...register('passwordConfirmation'), required: { value: true, message: 'This is required', validate: (value: string) => {*/}
-        {/*           if (watch && watch('password') !== value) {*/}
-        {/*           return 'Your passwords do no match';*/}
-        {/*       }*/}
-        {/*       }, }} />*/}
-        {/*<div>{<span>{errors.passwordConfirmation?.message}</span>}</div>*/}
-        {/*<button*/}
-        {/*    type="button"*/}
-        {/*    onClick={() => {}}*/}
-        {/*>Create new password</button>*/}
-      </form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>New password</label>
+          <input {
+              ...register('password',
+                  {required: { value: true, message: 'This is required'},
+                      maxLength: { value: MAX_LENGTH, message: `Max length exceeded, ${MAX_LENGTH} symbols`},
+                      minLength: { value: MIN_LENGTH, message: `Max length exceeded, ${MIN_LENGTH} symbols`},
+                  })
+                   // ,pattern
+          }
+          />
+          <div style={{width: '200px', color: 'red'}}>{ <span> Error: {errors.password?.message}</span>}</div>
+
+          <label>Password confirmation</label>
+          <input
+              {...register('passwordConfirmation',
+                  { required: { value: true, message: 'This is required'},
+                    maxLength: { value: MAX_LENGTH, message: `Max length exceeded, ${MAX_LENGTH} symbols`},
+                    minLength: { value: MIN_LENGTH, message: `Min length is ${MIN_LENGTH} symbols`},
+                   validate: (value: string) => {
+                    //@ts-ignore
+                      if (watch && watch('password') !== value) {
+                      return 'Your passwords do no match';
+                      }},
+                    // pattern
+                  }
+              )
+            }
+          />
+          <div style={{width: '200px', color: 'red'}}>{<span> Error: {errors.passwordConfirmation?.message}</span>}</div>
+          <button type="submit" onClick={() => {}}>
+            Create new password
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default CreateNewPassword
+export default CreateNewPassword;
