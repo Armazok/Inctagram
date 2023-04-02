3c9c6c774b2d01954f410504019aee3ba06e5e09
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-04-02 04:11:55 +0300

    style: stylization header and register

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
 import style from './Header.module.scss'
+import containerBlock from '../../styles/container.module.scss'
 
 export const Header = () => {
-  return (
-    <header className={style.header}>
-      <div>Inctagram</div>
-    </header>
-  )
+    return (
+        <div className={containerBlock.container}>
+            <header className={style.header}>
+                <div className={style.title}>Inctagram</div>
+            </header>
+        </div>
+    )
 }
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,41 +1,63 @@
 import React from 'react'
-
+import containerBlock from '../../styles/container.module.scss'
 import styleContainer from './container.module.scss'
 import style from './register.module.scss'
+import Heading from "@/components/Heading";
+import Image from "next/image";
+import facebook from "../../../public/icons/facebook-svgrepo.png"
+import google from "../../../public/icons/google-svgrepo.png"
+import Link from "next/link";
 
 export const Register = () => {
-  return (
-    <div className={`${style.formWrapper} ${styleContainer.container}`}>
-      <div className={style.formContainer}>
-        <h1 className={style.heading}>Sign Up</h1>
-        <form className={style.form}>
-          <div className={style.email}>
-            <label htmlFor={'email'} className={style.label}>
-              Email
-            </label>
-            <input id={'email'} name={'email'} type={'text'} />
-          </div>
+    return (
+        <div className={`${containerBlock.container} ${style.registerBlock}`}>
+            <div className={style.formContainer}>
+                <Heading style={style.title} tag={'h1'} text={'Sign Up'}/>
+                <div className={style.iconContainer}>
+                    <Link href={'https://www.google.com/'} target="_blank">
+                        <Image src={google} width={36} height={36} alt={'googleIcon'}/>
+                    </Link>
+                    <Link href={'https://www.facebook.com/'} target="_blank">
+                        <Image src={facebook} width={36} height={36} alt={'facebookIcon'}/>
+                    </Link>
+                </div>
 
-          <div className={style.password}>
-            <label htmlFor={'password'} className={style.label}>
-              Password
-            </label>
-            <input id={'password'} name={'password'} type={'password'} />
-            <div className={style.showHidePassword}></div>
-          </div>
+                <form className={style.form}>
+                    <div className={style.inputContainer}>
+                        <label htmlFor={'email'} className={style.label}>
+                            Email
+                        </label>
+                        <input id={'email'} name={'email'} type={'text'}/>
+                    </div>
 
-          <div className={style.password}>
-            <label htmlFor={'confirmPassword'} className={style.label}>
-              Confirm password
-            </label>
-            <input id={'confirmPassword'} name={'confirmPassword'} type={'confirmPassword'} />
-            <div className={style.showHidePassword}></div>
-          </div>
+                    <div className={style.inputContainer}>
+                        <label htmlFor={'password'} className={style.label}>
+                            Password
+                        </label>
+                        <input id={'password'} name={'password'} type={'password'}/>
+                        <div className={style.showHidePassword}></div>
+                    </div>
 
-          <button>Sign In</button>
-        </form>
-        <div className={style.account}>Don`t have an account?</div>
-      </div>
-    </div>
-  )
+                    <div className={style.inputContainer}>
+                        <label htmlFor={'confirmPassword'} className={style.label}>
+                            Confirm password
+                        </label>
+                        <input id={'confirmPassword'} name={'confirmPassword'} type={'confirmPassword'}/>
+                        {/*<div className={style.showHidePassword}></div>*/}
+                        <div className={style.forgotPassword}>
+                            <Link href={''}>Forgot Password</Link>
+                        </div>
+                    </div>
+                    {/*<Button text={'Sign In'} tag={'h3'}/>*/}
+                    {/*<button>Sign In</button>*/}
+                </form>
+                <div className={style.desciptionAccount}>
+                    <Link href={''}>Don`t have an account?</Link>
+                </div>
+                <div>
+                    <Link href={''}><Heading style={style.desciptionSignIn} tag={'h3'} text={'Sign In'}/></Link>
+                </div>
+            </div>
+        </div>
+    )
 }
