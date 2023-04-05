7ed5f2b47ab7b464998fc4cac3fcaf2c0b60e36f
demos461 <iamdemos461@gmail.com>
2023-04-05 16:16:43 +0300

    feat: add useRegistrationEmailResendingMutation and useRegistrationConfirmationQuery hooks

--- src/services/api/auth/hoook.ts
+++ src/services/api/auth/hoook.ts
@@ -1,6 +1,7 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { authAPI } from '@/services/api/auth/authAPI'
+import { noRefetch } from "@/helpers/no-refetch";
 
 export const useLoginMutation = () => {
   return useMutation({
@@ -12,4 +13,20 @@ export const useCreateNewPasswordMutation = () => {
   return useMutation({
     mutationFn: authAPI.createNewPassword,
   })
+}
+
+export const useRegistrationEmailResendingMutation = (email: string) => {
+  return useMutation({
+    mutationFn: () => authAPI.registrationEmailResending({ email }),
+  });
+}
+
+export const useRegistrationConfirmationQuery = (confirmationCode: string) => {
+  return useQuery({
+    queryKey: ["regConfirmation"],
+    queryFn: () => authAPI.registrationConfirmation({ confirmationCode}),
+    enabled: confirmationCode !== "undefined",
+    retry: false,
+    ...noRefetch
+  });
 }
