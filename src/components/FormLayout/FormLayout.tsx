603c18bc7d8a1e371ff767d31977975bbe6e5666
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-04-06 17:16:50 +0300

    style(form-layout): fix border

--- src/components/FormLayout/FormLayout.tsx
+++ src/components/FormLayout/FormLayout.tsx
@@ -7,7 +7,7 @@ type PropsType = {
 const FormLayout: FC<Partial<PropsType>> = ({ children, className }) => {
   return (
     <div
-      className={`container flex flex-col items-center w-full max-w-sm bg-secondBgColor ${className}`}
+      className={`container flex flex-col items-center w-full max-w-sm bg-secondBgColor rounded-[2px] ${className}`}
     >
       <div className="flex flex-col justify-center items-center w-full pt-[23px] pr-[24px] pb-[42px] pl-[24px] ">
         {children}
