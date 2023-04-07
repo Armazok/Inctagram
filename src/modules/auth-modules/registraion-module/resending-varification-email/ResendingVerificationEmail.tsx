d03e8ae2208a9675b06758511f9cd1220df43549
Klaus <nikolai.borsin@mail.ru>
2023-04-07 15:23:02 +0300

    ref: code query

--- src/modules/auth-modules/registraion-module/resending-varification-email/ResendingVerificationEmail.tsx
+++ src/modules/auth-modules/registraion-module/resending-varification-email/ResendingVerificationEmail.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { FC } from 'react'
 
+import { jsx } from '@storybook/theming'
+
+import IntrinsicAttributes = jsx.JSX.IntrinsicAttributes
 import { PATH_ROUTE } from '@/common/constants/PATH_ROUTE'
 import Preloader from '@/components/atoms/preloader/Preloader'
 import EmailSuccessMessage from '@/components/AuthComponents/email-success-message/EmailSuccesMessage'
 import ResendingVerificationLink from '@/components/AuthComponents/resending-verification-link/ResendingVerificationLink'
 import { useConfirmationQuery } from '@/modules/auth-modules/registraion-module/resending-varification-email/api/confirmationRequest'
 
-const ResendingVerificationEmail = (props: any) => {
-  /* const {
-    query: { code },
-  } = useRouter()*/
-  const { isLoading, isError } = useConfirmationQuery(props as string)
+type resType = {
+  code: string | string[] | undefined
+}
+
+const ResendingVerificationEmail: FC<resType> = ({ code }) => {
+  const { isLoading, isError } = useConfirmationQuery(code as string)
 
   if (isLoading) return <Preloader />
 
--- src/pages/auth/registration-confirmation/index.tsx
+++ src/pages/auth/registration-confirmation/index.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
 
+import { useRouter } from 'next/router'
+
 import ResendingVerificationEmail from '@/modules/auth-modules/registraion-module/resending-varification-email/ResendingVerificationEmail'
 
 const RegistrationConfirmation: FC = () => {
-  /* const {
+  const {
     query: { code },
-  } = useRouter()*/
-  return <ResendingVerificationEmail code={'' /*code*/} />
+  } = useRouter()
+
+  return <ResendingVerificationEmail code={code} />
 }
 
 export default RegistrationConfirmation
