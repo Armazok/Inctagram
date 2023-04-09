2860a7235b2096ba8deda706ae38ac11c2df41a2
Nikolay <nikolai.borsin@mail.ru>
2023-04-09 13:36:33 +0300

    Login (#22)
    * feat: eye img
    
    * feat: add provider tanstack
    
    * feat: add instanse + api
    
    * feat: add instanse + api
    
    * feat: add formSubmit
    
    * ref: name props
    feat: add img eye
    
    * feat: add res + req type + index export
    
    * ref: cli fix
    
    * feat: add store an email, modalWindow
    
    * feat: add store an email, modalWindow
    
    * rebase
    
    * rebase
    
    * feat:added division into blocks in the login component
    ref: rewritten styles, got rid of scss
    
    * cli fix
    
    * ref: ref
    
    * ref: ref
    
    * ref: ref
    
    * ref: story ignor
    
    * ref: ref
    
    * ref: ref
    
    * ref: ref

---
+++ src/modules/blockFormLogin/blockLoginForm.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+
+import Preloader from '@/components/atoms/preloader/Preloader'
+import { useLoginMutation } from '@/services/api/auth/hoook'
+import GlobalButton from '@/ui/buttons/GlobalButton'
+import GlobalInput from '@/ui/Inputs/Input/Input'
+import InputWithEye from '@/ui/Inputs/InputWithEye/InputWithEye'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+export const BlockLoginForm = () => {
+  const router = useRouter()
+  const { mutate: login, isLoading } = useLoginMutation()
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<Inputs>()
+
+  const handleFormSubmit = async ({ email, password }: Inputs) => {
+    await login({
+      email,
+      password,
+    })
+    router.push('./TEST_PAGE')
+  }
+
+  if (isLoading) return <Preloader />
+
+  return (
+    <>
+      {isLoading && <Preloader />}
+      <form
+        className="flex flex-col grow gap-[10px] pt-[22px]  pb-[18px] w-full gap-[24px]"
+        onSubmit={handleSubmit(handleFormSubmit)}
+      >
+        <GlobalInput
+          type="email"
+          id="email"
+          placeholder="Email"
+          label="Email"
+          error={errors?.email?.message}
+          {...register('email')}
+        />
+        <InputWithEye
+          label="Password"
+          id="password"
+          placeholder="Password"
+          error={errors?.password?.message}
+          {...register('password')}
+        />
+        <Link href={'/auth/forgot-password'} className={'flex justify-end text-light-900 text-xs'}>
+          Forgot password?
+        </Link>
+        <GlobalButton variant="default" type="submit">
+          Sign In
+        </GlobalButton>
+      </form>
+    </>
+  )
+}
