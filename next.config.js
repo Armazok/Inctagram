4b1f8e624f5c536838ea67e3b76df00a112d73a5
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-04-09 16:36:43 +0300

    build(package): removed unused dependencies

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
-const path = require('path')
-
 const withTM = require('next-transpile-modules')(['tailwindcss'])
 
 const nextConfig = withTM({
   reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
   images: {
     unoptimized: true,
-  },
-  outDir: '.next',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    tsconfigPaths: [path.join(__dirname, 'tsconfig.json')],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        child_process: false,
-        fs: false,
-        module: false,
-      }
-    }
-
-    return config
   },
 })
 
