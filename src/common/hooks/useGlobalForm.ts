3edee3de34a164f9839c3c8728b64db653b837e6
Klaus <nikolai.borsin@mail.ru>
2023-04-10 11:43:14 +0300

    feat: add global Hook useLocalStorage

--- src/common/hooks/useGlobalForm.ts
+++ src/common/hooks/useGlobalForm.ts
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
 
@@ -26,4 +28,26 @@ export const useGlobalForm = (schema: any) => {
     errors,
     setCustomError,
   }
+}
+
+export function useLocalStorage(key: string, initialValue: string) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key)
+
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error)
+
+      return initialValue
+    }
+  })
 }
