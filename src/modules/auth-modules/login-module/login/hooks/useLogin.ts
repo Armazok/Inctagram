270862f644cfa59848bf18b1f9817a4bdb0bfbb7
Klaus <nikolai.borsin@mail.ru>
2023-04-10 11:44:14 +0300

    ref: toast state feat: add custom validation

--- src/modules/auth-modules/login-module/login/hooks/useLogin.ts
+++ src/modules/auth-modules/login-module/login/hooks/useLogin.ts
@@ -1,9 +1,13 @@
 import { useMutation } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
 
 import { sendLoginRequest } from '@/modules/auth-modules/login-module/login/api/loginAPI'
-import { sendRegisterRequest } from '@/modules/auth-modules/registraion-module/registration/api/sendRegisterRequest'
 
-export const useLoginMutation = (onSuccess?: any, setCustomError?: any, reset?: any) => {
+export const useLoginMutation = (
+  onSuccess: () => void,
+  setCustomError: () => void,
+  reset: () => void
+) => {
   const {
     data,
     isLoading,
@@ -14,13 +18,12 @@ export const useLoginMutation = (onSuccess?: any, setCustomError?: any, reset?: 
     mutationKey: ['login'],
     onSuccess: () => {
       onSuccess()
-
-      // reset()
+      reset()
+      toast.success('Success')
     },
-    onError: error => {
-      // @ts-ignore
-      setCustomError('email', `${error?.response?.data?.messages[0].message}`)
-      // setCustomError()
+    onError: () => {
+      setCustomError()
+      toast.error('Error')
     },
   })
 
