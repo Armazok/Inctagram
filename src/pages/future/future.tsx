24bb73fe27ca6aedafdb467abbf07b05b8762374
Nikolay <nikolai.borsin@mail.ru>
2023-04-10 21:47:59 +0300

    feat: add tabs component (#33)

--- src/pages/future/future.tsx
+++ src/pages/future/future.tsx
@@ -1,13 +1,22 @@
 import React, { FC, memo } from 'react'
 
+import { LoginPage, RegisterPage } from '@/modules'
+import TabComponent from '@/ui/tabs/GlobalTabs'
+
 interface IFuture {}
 
 const Future: FC<IFuture> = memo(({}) => {
   return (
-    <div className={'flex flex-col text-danger-300 items-center mt-48 uppercase'}>
-      The future is ours
+    <div>
+      <TabComponent
+        tabComponent1={<LoginPage />}
+        nameTab1={'General information'}
+        tabComponent2={<RegisterPage />}
+        nameTab2={'Devices'}
+      />
     </div>
   )
 })
 
+// className={'flex flex-col text-danger-300 items-center mt-48 uppercase'}
 export default Future
