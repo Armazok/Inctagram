c8fb53f40e70aa949de8ca7bd3118b51cb50438d
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-04-12 01:50:42 +0300

    fix: date picker

---
+++ src/ui/date-picker/DatePicker.tsx
@@ -0,0 +1,106 @@
+import { ComponentProps, FC } from 'react'
+
+import clsx from 'clsx'
+import { ReactDatePicker } from 'react-datepicker'
+
+import 'react-datepicker/dist/react-datepicker.css'
+import s from '../date-picker/datePicker.module.scss'
+
+import { CustomHeader } from '@/ui/date-picker/custom/CustomHeader'
+import { CustomInput } from '@/ui/date-picker/custom/CustomInput'
+
+type CommonProps = {
+  placeholder?: string
+  startDate: Date | null
+  setStartDate: (date: Date | null) => void
+  label?: string
+  error?: boolean
+  errorMessage?: string
+  disabled?: boolean
+} & ComponentProps<'div'>
+
+type ConditionalProps =
+  | {
+      endDate: Date | null
+      setEndDate: (date: Date | null) => void
+    }
+  | {
+      endDate: never
+      setEndDate: never
+    }
+
+export type DatePickerProps = CommonProps & ConditionalProps
+
+const DateCalendar: FC<DatePickerProps> = ({
+  startDate,
+  setStartDate,
+  placeholder,
+  label,
+  error,
+  errorMessage,
+  endDate,
+  setEndDate,
+  disabled,
+  ...rest
+}) => {
+  const classNames = {
+    input: clsx(s.blockContainer, error && s.errorBlockContainer),
+    calendar: s.calendar,
+    popper: s.popper,
+    errorText: clsx(error && s.errorText),
+    day: () => s.day,
+  }
+
+  const isRange = endDate !== undefined
+
+  const DatePickerHandler = (dates: [Date | null, Date | null] | Date | null) => {
+    if (Array.isArray(dates)) {
+      const [start, end] = dates
+
+      setStartDate(start)
+      setEndDate(end)
+    } else {
+      setStartDate(dates)
+    }
+  }
+
+  return (
+    <div {...rest}>
+      <ReactDatePicker
+        dateFormat="dd-MM-yyyy"
+        startDate={startDate}
+        endDate={endDate}
+        selected={startDate}
+        preventOpenOnFocus={true}
+        selectsRange={isRange}
+        renderCustomHeader={({ date, decreaseMonth, increaseMonth, ...rest }) => (
+          <CustomHeader
+            date={date}
+            decreaseMonth={decreaseMonth}
+            increaseMonth={increaseMonth}
+            {...rest}
+          />
+        )}
+        onChange={(dates: [Date | null, Date | null] | Date | null) => DatePickerHandler(dates)}
+        customInput={<CustomInput isRange={isRange} label={label} error={true} disabled={true} />}
+        dayClassName={classNames.day}
+        calendarClassName={classNames.calendar}
+        className={classNames.input}
+        popperClassName={classNames.popper}
+        showPopperArrow={false}
+        calendarStartDay={1}
+        popperModifiers={[
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 0],
+            },
+          },
+        ]}
+      />
+      {error && <span className={classNames.errorText}>{errorMessage}</span>}
+    </div>
+  )
+}
+
+export default DateCalendar
