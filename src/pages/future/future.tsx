c70df26831c7746b90cc255c49a8962452927cf1
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-04-13 02:08:41 +0300

    fix: global_tabs and confirm

--- src/pages/future/future.tsx
+++ src/pages/future/future.tsx
@@ -1,19 +1,11 @@
 import React, { FC, memo } from 'react'
 
-import { LoginPage, RegisterPage } from '@/modules'
-import TabComponent from '@/ui/tabs/GlobalTabs'
-
 interface IFuture {}
 
 const Future: FC<IFuture> = memo(({}) => {
   return (
     <div>
-      <TabComponent
-        tabComponent1={<LoginPage />}
-        nameTab1={'General information'}
-        tabComponent2={<RegisterPage />}
-        nameTab2={'Devices'}
-      />
+      <h1>Future</h1>
     </div>
   )
 })
