e1b4f11c6802972cdeb01791d4928fc762d23a1e
Anna <anna.blackbird1@gmail.com>
2023-04-13 17:27:59 +0700

    fix: update styles. add set avatar from data

--- src/modules/profile-modules/avatar-module/api/avatar-api.ts
+++ src/modules/profile-modules/avatar-module/api/avatar-api.ts
@@ -1,16 +1,10 @@
 import { authInstance } from '@/services/api/auth/instanse'
 
 export const sendAvatar = (formData: File) => {
-  debugger
-
   return authInstance.post<ResAvatarType>('users/profile/avatar', formData, {
     headers: {
-      // 'Content-Type': 'application/octet-stream',
       'Content-Type': 'multipart/form-data',
-      // 'Content-Length': '',
-      Host: 'lionfish-app-3jdhn.ondigitalocean.app',
-      // 'Cross-Domain': 'true',
-      // accept: 'application/json',
+      // Host: 'lionfish-app-3jdhn.ondigitalocean.app',
     },
   })
 }
