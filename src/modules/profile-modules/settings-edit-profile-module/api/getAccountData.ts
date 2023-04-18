6a773eadd62c140ed88c0c20fba3b708636f6f07
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-04-18 17:45:04 +0300

    refactor(get-profile-data): del id

--- src/modules/profile-modules/settings-edit-profile-module/api/getAccountData.ts
+++ src/modules/profile-modules/settings-edit-profile-module/api/getAccountData.ts
@@ -1,5 +1,6 @@
+import { RootProfile } from '@/modules/profile-modules/settings-edit-profile-module/types/profile-type'
 import { authInstance } from '@/services'
 
-export const getAccountData = (id: number | undefined) => {
-  return authInstance.get(`users/profile/${id}`)
+export const getAccountData = (): Promise<RootProfile> => {
+  return authInstance.get(`users/profile`)
 }
