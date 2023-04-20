a4e5c33576619023ad573bdbec14ef8292735efe
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-04-20 16:12:10 +0300

    refactor(instance): add response intereseptor (in the process improvements!!)

--- src/services/api/instanse.ts
+++ src/services/api/instanse.ts
@@ -5,8 +5,48 @@ export const authInstance = axios.create({
   withCredentials: true,
 })
 
-authInstance.interceptors.request.use(config => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
+authInstance.interceptors.request.use(
+  config => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
 
-  return config
-})
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+// authInstance.interceptors.response.use(
+//   response => response,
+//   async error => {
+//     const originalRequest = error.config
+//
+//     console.log(error, 'error')
+//     console.log(originalRequest, 'originalRequest')
+//     if (!originalRequest._isRetry && error?.response?.data?.statusCode === 401) {
+//       originalRequest._isRetry = true
+//
+//       console.log(originalRequest, 'originalRequest - 2')
+//
+//       try {
+//         const response = await axios.post(
+//           `https://lionfish-app-3jdhn.ondigitalocean.app/auth/update-tokens`,
+//           {},
+//           { withCredentials: true }
+//         )
+//
+//         console.log(response, 'response')
+//
+//         localStorage.setItem('accessToken', accessToken in response )
+//
+//         originalRequest.headers['Authorization'] = `Bearer ${accessToken in response}`
+//
+//         return authInstance(originalRequest)
+//       } catch (e) {
+//         console.log(e, 'e')
+//         //localStorage.removeItem('accessToken')
+//
+//       }
+//     }
+//
+//     return Promise.reject(error)
+//   }
+// )
