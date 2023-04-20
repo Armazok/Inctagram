a8420bde98de78cf92f21e26a4d2e7cdecda591b
Klaus <nikolai.borsin@mail.ru>
2023-04-20 17:01:37 +0300

    ref: ref config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -5,6 +7,13 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-mdx-gfm',
+    '@storybook-addon-next',
+    {
+      name: '@storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -12,5 +21,8 @@ module.exports = {
   },
   docs: {
     autodocs: true,
+  },
+  typescript: {
+    reactDocgen: false,
   },
 }
