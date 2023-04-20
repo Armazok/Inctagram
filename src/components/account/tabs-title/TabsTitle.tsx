276bb0e5812f01b4f457f8bc8534eea8000dfeb8
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-04-20 16:10:00 +0300

    refactor(tabs-title): removed unnecessary props and checks

--- src/components/account/tabs-title/TabsTitle.tsx
+++ src/components/account/tabs-title/TabsTitle.tsx
@@ -9,36 +9,25 @@ interface TabsType {
 }
 
 interface PropsTabType {
-  variant: 'create' | 'edit'
   tabs?: TabsType[]
   activeTab?: string
   setActiveTab?: (activeTab: string | undefined) => void
 }
 
-export const TabsTitle: FC<PropsTabType> = ({ variant, tabs, setActiveTab, activeTab }) => {
+export const TabsTitle: FC<PropsTabType> = ({ tabs, setActiveTab, activeTab }) => {
   return (
     <>
-      {variant === 'edit' && (
-        <>
-          <div className="flex border-b border-gray-200  w-[313px] border-none h-[96] gap-[2px]">
-            {tabs?.map(tab => (
-              <TabComponent
-                key={tab.id}
-                label={tab.label}
-                activeTab={activeTab}
-                setActiveTab={setActiveTab!}
-              />
-            ))}
-          </div>
-          <div className="divide-y-[100%] bg-bgLogBorder h-[1px]"></div>
-        </>
-      )}
-      {variant === 'create' && (
-        <>
-          <h1 className="text-white pb-[12px]">Create Profile</h1>
-          <div className="divide-y-[100%] bg-bgLogBorder h-[1px]"></div>
-        </>
-      )}
+      <div className="flex border-b border-gray-200  border-none h-[96] gap-[2px]">
+        {tabs?.map(tab => (
+          <TabComponent
+            key={tab.id}
+            label={tab.label}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab!}
+          />
+        ))}
+      </div>
+      <div className="divide-y-[100%] bg-bgLogBorder h-[1px]"></div>
     </>
   )
 }
