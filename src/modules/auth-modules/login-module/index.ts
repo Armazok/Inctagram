a7683383029b6dc41d75e846a6571cba1aba08a2
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-05-15 01:05:04 +0300

    refactor(public-app): add path

--- src/modules/auth-modules/login-module/index.ts
+++ src/modules/auth-modules/login-module/index.ts
@@ -4,3 +4,4 @@ export * from './login/hooks/useLogin'
 export * from './login/Login'
 
 export { LoginForm } from './login/components/LoginForm'
+export { GoogleGithubLogin } from './login/components/GoogleGithubLogin'
