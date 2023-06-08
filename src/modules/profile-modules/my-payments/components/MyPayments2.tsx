1735668c3ccba8bd3adad073b2a90984007f29f0
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-06-08 14:06:21 +0300

    css: added a small table styling

--- src/modules/profile-modules/my-payments/components/MyPayments2.tsx
+++ src/modules/profile-modules/my-payments/components/MyPayments2.tsx
@@ -8,6 +8,8 @@ import {
 } from '@tanstack/react-table'
 import { useTable } from 'react-table'
 
+import s from './index.module.scss'
+
 import { capitalizeFirstLetter } from '@/common'
 import {
   dateChangesFormat,
@@ -25,27 +27,27 @@ export const MyPayments2 = () => {
     () => [
       {
         accessor: 'dateOfPayment',
-        header: 'Date of Payment',
+        Header: 'Date of Payment',
         Cell: (params: any) => dateChangesFormat(params.value),
       },
       {
         accessor: 'endDateOfSubscription',
-        header: 'End date of subscription',
+        Header: 'End date of subscription',
         Cell: (params: any) => dateChangesFormat(params.value),
       },
       {
         accessor: 'price',
-        header: 'Price',
+        Header: 'Price',
         Cell: (params: any) => '$' + params.value,
       },
       {
         accessor: 'subscriptionType',
-        header: 'Subscription Type',
+        Header: 'Subscription Type',
         Cell: (params: any) => capitalizeFirstLetter(params.value),
       },
       {
         accessor: 'paymentType',
-        header: 'Payment Type',
+        Header: 'Payment Type',
         Cell: (params: any) => capitalizeFirstLetter(params.value),
       },
     ],
@@ -58,35 +60,39 @@ export const MyPayments2 = () => {
   })
 
   return (
-    <div>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            // eslint-disable-next-line react/jsx-key
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
+    <>
+      <div className="text-light-100 p-2 block max-w-full overflow-x-scroll overflow-y-hidden">
+        <div className={s.container}>
+          <table className="w-full " {...getTableProps()}>
+            <thead>
+              {headerGroups.map(headerGroup => (
                 // eslint-disable-next-line react/jsx-key
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    // eslint-disable-next-line react/jsx-key
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row)
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map(row => {
+                prepareRow(row)
 
-            return (
-              // eslint-disable-next-line react/jsx-key
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => (
+                return (
                   // eslint-disable-next-line react/jsx-key
-                  <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
-                ))}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => (
+                      // eslint-disable-next-line react/jsx-key
+                      <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                    ))}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   )
 }
