12bf743031155051e3cdb1434e9068be4010a7d7
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-06-19 17:06:53 +0300

    css: add skeleton for my payments

--- src/modules/profile-modules/my-payments/custom/setMyPaymentsDataEffect.ts
+++ src/modules/profile-modules/my-payments/custom/setMyPaymentsDataEffect.ts
@@ -3,11 +3,13 @@ import { useEffect } from 'react'
 export const setMyPaymentsDataEffect = (
   data: any,
   isSuccess: boolean,
+  isLoading: boolean,
   setMyPaymentsData: (data: any) => any
 ) => {
   return useEffect(() => {
+    isLoading || !isSuccess ? setMyPaymentsData(Array(30).fill({})) : data
     if (data) {
       setMyPaymentsData(data)
     }
-  }, [isSuccess])
+  }, [isSuccess, isLoading])
 }
